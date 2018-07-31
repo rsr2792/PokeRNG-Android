292a74939aedf831f83935133b900b2688546ba2
rsr2792 <rsr2792alt@gmail.com>
2018-07-30 21:58:06 -0700

    First commit (fixed)
    Errors with previous commit and git setup.

---
+++ PokeRNG/app/src/main/java/www/legendary75/org/pokerng/PokeSearchActivity.java
@@ -0,0 +1,13 @@
+package www.legendary75.org.pokerng;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PokeSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poke_search);
+    }
+}
